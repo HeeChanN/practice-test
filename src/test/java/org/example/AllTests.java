a71a14e2cf4537d80cc11f10b73ad034f04c3038
Ahnheechan <ahc70032@gmail.com>
2023-12-27 13:04:22 +0900

    스위트 실습

---
+++ src/test/java/org/example/AllTests.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(value = Suite.class)
+@Suite.SuiteClasses(value = {
+        CalculatorTest.class,
+        ParameterizedTest.class})
+public class AllTests {
+}
+
+
+/**
+ *  Suite는 복수의 테스트를 묶을 수 있는 집합
+ *  테스트 클래스 내의 모든 @Test를 찾아 실행하는 러너이기도 하다.
+ *  이렇게 현재 만들어 놓은 2개의 테스트를 묶어서 한번에 테스트를 진행할 수 있다.
+ *
+ * */
